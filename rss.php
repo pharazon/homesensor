088963c2c1cb6cf186dceb22289a0c29eac2dc82
Mikko Vartiainen <mvartiainen@gmail.com>
2012-10-07 21:44:29 +0300

    use Sensor class from temperature.php

--- rss.php
+++ rss.php
@@ -1,29 +1,7 @@
 <?php
+require "temperature.php";
 
-class Sensor
-{
-	public $id;
-	public $name;
-	public $value;
-}
-
-$database =  "Lampo";
-$dbconnect = mysql_connect(localhost, root, '');
-mysql_select_db($database, $dbconnect);
-$query = "select Anturi, nimi from Anturit";
-$result = mysql_query($query, $dbconnect);
-
-for ($i=0; $line = mysql_fetch_assoc($result); $i++)
-{
-	$s = new Sensor();
-	$s->name = $line['nimi'];
-	$s->id = $line['Anturi'];
-	$q = mysql_query("select Lampotila from Mittaukset where Anturi =".$line['Anturi']." order by Aika desc limit 1" , $dbconnect);
-	$row = mysql_fetch_row($q);
-	$s->value = $row[0];
-	$sensors[$i] = $s;
-}
-
+$sensors = Sensor::get_sensor_array();
 $now = date("D, d M Y H:i:s T");
 
 $output = "<?xml version=\"1.0\"?>
