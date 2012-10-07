1fd665845c1814763641c39fa5927b5c1f16dfdc
Mikko Vartiainen <mvartiainen@gmail.com>
2012-10-07 23:45:26 +0300

    add dynamic paths

--- rss.php
+++ rss.php
@@ -10,18 +10,22 @@ $sensors = Sensor::get_sensor_array();
 $now = date("D, d M Y H:i:s T");
 
 $clientip = $_SERVER['REMOTE_ADDR'];
-if (!is_ip_private($clientip) and !is_ip_private($_SERVER['SERVER_ADDR']))
+if (isset($_SERVER['SERVER_NAME']))
+    $ip = $_SERVER['SERVER_NAME'];
+elseif (!is_ip_private($clientip) and !is_ip_private($_SERVER['SERVER_ADDR']))
     $ip = $_SERVER['SERVER_ADDR'];
 elseif (!is_ip_private($clientip) and is_ip_private($_SERVER['SERVER_ADDR']))
     $ip = file_get_contents("http://ifconfig.me/ip");
 else
     $ip = $_SERVER['SERVER_ADDR'];
 
+$path = $dirpath = substr($_SERVER['REQUEST_URI'],1,strrpos($_SERVER['REQUEST_URI'],'/')-1);
+
 $output = "<?xml version=\"1.0\"?>
 <rss version=\"2.0\">
   <channel>
   <title>Lämpötila</title>
-  <image>http://192.168.1.2/lampotila/graph.php</image>
+  <image>http://".$ip."/".$path."/graph.php</image>
   <pubDate>$now</pubDate>
   <lastBuildDate>$now</lastBuildDate>
   <ttl>5</ttl>\n";
@@ -31,7 +35,7 @@ foreach ($sensors as $sensor)
     $output .= "  <item>
     <title>".$sensor->name."</title>
     <description>".$sensor->value."</description>
-    <link>http://".$ip."/lampotila/graph.php?".htmlspecialchars("sensor[]=$sensor->id")."</link>
+    <link>http://".$ip."/".$path."/graph.php?".htmlspecialchars("sensor[]=$sensor->id")."</link>
     <sensor>".$sensor->id."</sensor>
   </item>\n";
 }
