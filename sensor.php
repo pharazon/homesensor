e663252262effc0dea174b685ac54f2a22b6ea8b
Mikko Vartiainen <mvartiainen@gmail.com>
2012-10-10 23:36:41 +0300

    use correct header

--- sensor.php
+++ sensor.php
@@ -3,7 +3,7 @@ require "temperature.php";
 
 $sensors = Sensor::get_sensor_array();
 
-header('Content-type: text/json');
+header('Content-type: application/json');
 echo json_encode($sensors);
 
 ?>
